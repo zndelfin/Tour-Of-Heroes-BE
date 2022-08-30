fb43f909e70e3db64b0c606aaa9b414c22d8d76c
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-08-30 08:36:17 +0800

    TECH-217: Added supertest and nestjs/testing dependencies

--- src/characters/characters.service.ts
+++ src/characters/characters.service.ts
@@ -1,68 +1,65 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { CharactersModel } from './characters.model';
-import { v4 as uuid } from "uuid";
+import { v4 as uuid } from 'uuid';
 
 @Injectable()
 export class CharactersService {
-  
-    initialValues = [
-      {
-          "id": "1",
-          "name": "Aslaug",
-          "description": "warrior queen"
-      },
-  
-      {
-          "id": "2",
-          "name": "Ivar the Boneless",
-          "description": "commander of the Great Heathen Army"
-      },
-      
-      
-      {
-          "id": "3",
-          "name": "Lagertha the Sheildmaiden",
-          "description": "aka Hlaógerór"
-      },
-      
-      
-      {
-          "id": "4",
-          "name": "Ragnar Lothbrok",
-          "description": "aka Ragnard Sigurdsson"
-      }
+  initialValues = [
+    {
+      id: '1',
+      name: 'Aslaug',
+      description: 'warrior queen'
+    },
+
+    {
+      id: '2',
+      name: 'Ivar the Boneless',
+      description: 'commander of the Great Heathen Army'
+    },
+
+    {
+      id: '3',
+      name: 'Lagertha the Sheildmaiden',
+      description: 'aka Hlaógerór'
+    },
+
+    {
+      id: '4',
+      name: 'Ragnar Lothbrok',
+      description: 'aka Ragnard Sigurdsson'
+    }
   ];
 
   private characters: CharactersModel[] = this.initialValues;
 
-    addCharacter(name: string, desc: string) {
-        const charId = uuid();
-        const newCharacter = new CharactersModel(charId, name, desc);
-        this.characters.push(newCharacter);
-        return charId;
-      }
+  addCharacter(name: string, desc: string) {
+    const charId = uuid();
+    const newCharacter = new CharactersModel(charId, name, desc);
+    this.characters.push(newCharacter);
+    return charId;
+  }
 
-    getCharacters() {
-        return [...this.characters];
-      }
-    
-    getSingleCharacter(characterId: string) {
-      return this.characters.find(character => character.id === characterId)
+  getCharacters() {
+    return [...this.characters];
+  }
+
+  getSingleCharacter(characterId: string) {
+    return this.characters.find((character) => character.id === characterId);
+  }
+
+  updateCharacter(characterId: string, name: string, desc: string) {
+    const character = this.characters.find((character) => character.id === characterId);
+    if (name) {
+      character.name = name;
     }
-    
-      updateCharacter(characterId: string, name: string, desc: string) {
-        const character = this.characters.find(character => character.id === characterId)
-        if (name) {
-          character.name = name;
-        }
-        if (desc) {  
-          character.description = desc;
-        }
-        return character;
-      }
-    
-      deleteCharacter(charId: string) {
-          const filtered = this.characters.filter(character=> charId !== character.id)
-          return this.characters = filtered;
-      }
+    if (desc) {
+      character.description = desc;
     }
+    return character;
+  }
+
+  deleteCharacter(charId: string) {
+    const filtered = this.characters.filter((character) => charId !== character.id);
+    return (this.characters = filtered);
+  }
+}

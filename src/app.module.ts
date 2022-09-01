38837ce40a07a2b207d2e067f9a753d901c8e60f
Zuleika Nyx Delfin <zdelfin@isbx.com>
2022-09-01 13:07:37 +0800

    TECH-220: Installed additional dotenv dependency and imported typeOrmModue to root AppModule

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { CharactersModule } from './characters/characters.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [CharactersModule]
